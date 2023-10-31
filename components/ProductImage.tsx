9dbea6299fa405eb54dffa875f4c25b871c19a22
Dan <daniel.speranskiy@live.com>
2023-10-31 13:22:42 +0400

    Squashing bugs for deployment

--- components/ProductImage.tsx
+++ components/ProductImage.tsx
@@ -28,11 +28,11 @@ function ProductImage({product,fill}:Props) {
         {fill ? (
             <Image src={product.image} alt={product.title} fill 
             className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${loading ? 'scale-110 blur-2xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
-            onLoadingComplete={() => setloading(false)}/>
+            onLoad={() => setloading(false)}/>
         ):(
             <Image src={product.image} alt={product.title} width={400} height={1000} 
             className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${loading ? 'scale-110 blur-2xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
-            onLoadingComplete={() => setloading(false)}/>
+            onLoad={() => setloading(false)}/>
         )}
     </>
   )
